c8898c70337384f6500e74d43f55213597594d13
Gabriel Victor <gabrielvictorweb@outlook.com>
2024-03-31 01:26:56 -0300

    feat(auth): jwt authentication

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dtos/AuthDto';
 
 @Controller('auth')
 export class AuthController {
@@ -17,8 +18,8 @@ export class AuthController {
 
     @HttpCode(HttpStatus.OK)
     @Post('login')
-    signIn(@Body() signInDto: Record<string, any>) {
-        return this.authService.signIn(signInDto.username, signInDto.password);
+    signIn(@Body() signInDto: AuthDto) {
+        return this.authService.signIn(signInDto);
     }
 
     @UseGuards(AuthGuard)
--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { AuthDto } from './dtos/AuthDto';
 
 @Injectable()
 export class AuthService {
@@ -9,12 +10,9 @@ export class AuthService {
         private jwtService: JwtService
     ) { }
 
-    async signIn(
-        username: string,
-        pass: string,
-    ): Promise<{ access_token: string }> {
-        const user = await this.usersService.findOne(username);
-        if (user?.password !== pass) {
+    async signIn(authData: AuthDto): Promise<{ access_token: string }> {
+        const user = await this.usersService.findOne(authData.username);
+        if (user?.password !== authData.password) {
             throw new UnauthorizedException();
         }
         const payload = { sub: user.id, username: user.name };
