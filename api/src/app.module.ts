5d3b0ef3ba4618d4fc81b0700b50117bd41d0ca6
Gabriel Victor <gabrielvictorweb@outlook.com>
2024-03-31 01:30:31 -0300

    chore: config modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { PurchasesService } from './purchases/purchases.service';
+import { PurchasesController } from './purchases/purchases.controller';
+import { PurchasesModule } from './purchases/purchases.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule, ProductsModule, PurchasesModule],
+  controllers: [AppController, PurchasesController],
+  providers: [AppService, PurchasesService],
 })
 export class AppModule { }
