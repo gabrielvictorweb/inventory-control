8d80db29b5788b87ff9e4714e0c0640edc106e21
Gabriel Victor <gabrielvictorweb@outlook.com>
2024-03-31 23:20:19 -0300

    feat(products): find, delete and update

--- api/src/products/products.controller.ts
+++ api/src/products/products.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductsDto } from './dtos/CreateProductsDto';
 import { Roles } from 'src/roles/roles';
 import { Role } from 'src/roles/role.enum';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { RolesGuard } from 'src/roles/role.guard';
+import { UpdateProductsDto } from './dtos/UpdateProductsDto';
 
 @Controller('products')
 export class ProductsController {
@@ -16,11 +17,33 @@ export class ProductsController {
         return this.productsService.findAll();
     }
 
+    @HttpCode(HttpStatus.OK)
+    @Get(':id')
+    findOne(@Param('id') id) {
+        return this.productsService.findOne(id);
+    }
+
     @Roles(Role.Admin)
     @UseGuards(AuthGuard, RolesGuard)
     @HttpCode(HttpStatus.CREATED)
     @Post()
     save(@Body() createProductsDto: CreateProductsDto) {
         return this.productsService.save(createProductsDto);
+    }
+
+    @Roles(Role.Admin)
+    @UseGuards(AuthGuard, RolesGuard)
+    @HttpCode(HttpStatus.OK)
+    @Delete(':id')
+    delete(@Param('id') id) {
+        return this.productsService.delete(id);
+    }
+
+    @Roles(Role.Admin)
+    @UseGuards(AuthGuard, RolesGuard)
+    @HttpCode(HttpStatus.OK)
+    @Patch(':id')
+    update(@Param('id') id, @Body() updateProductsDto: UpdateProductsDto) {
+        return this.productsService.update(updateProductsDto, id);
     }
 }
--- api/src/products/products.service.ts
+++ api/src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Products } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductsDto } from './dtos/CreateProductsDto';
+import { UpdateProductsDto } from './dtos/UpdateProductsDto';
 
 @Injectable()
 export class ProductsService {
@@ -11,7 +12,19 @@ export class ProductsService {
         return this.prisma.products.findMany();
     }
 
-    async save(product: CreateProductsDto): Promise<unknown> {
-        return this.prisma.products.create({ data: product });
+    async findOne(id: number): Promise<Products> {
+        return this.prisma.products.findUnique({ where: { id } });
+    }
+
+    async save(createProductsDto: CreateProductsDto): Promise<unknown> {
+        return this.prisma.products.create({ data: createProductsDto });
+    }
+
+    async update(updateProductsDto: UpdateProductsDto, id: number): Promise<unknown> {
+        return this.prisma.products.update({ where: { id }, data: updateProductsDto });
+    }
+
+    async delete(id: number): Promise<unknown> {
+        return this.prisma.products.delete({ where: { id } });
     }
 }
